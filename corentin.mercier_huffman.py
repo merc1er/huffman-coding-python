6755a29a8f8fc11b9025ca0c8b37affbdf399bdf
Kali <corentin.mercier@epita.fr>
2017-04-30 00:40:38 +0200

    Third function working

--- corentin.mercier_huffman.py
+++ corentin.mercier_huffman.py
@@ -99,11 +99,17 @@ def _buildHuffmanTree(L,x):
 ################################################################################
 ## ENCODE DATA USING THE TREE
 
-def encodeData(dataIN, huffmanTree): # Not working
+def encodeData(dataIN, huffmanTree):
     """
     Encodes the input string to its binary string representation.
     """
-    pass
+    dic = codeDict(huffmanTree)
+    string = ""
+    for i in dataIN:
+        for n in range(len(dic)):
+            if i == dic[n][0]:
+                string += dic[n][1]
+    return string
 
 def codeDict(tree):
     """
@@ -143,8 +149,6 @@ def compress(dataIn):
     """
     The main function that makes the whole compression process.
     """
-
-    # FIXME
     pass
 
 
@@ -188,102 +192,11 @@ def decompress(data, dataAlign, tree, treeAlign):
 ################################################################################
 ## TESTS
 
-# def encodeData(dataIN, huffmanTree):
-#   MT = _MappingTable(huffmanTree)
-#
-#   MessInBinHuff = ""
-#   for i in dataIN:
-#     for n in range (len(MT)):
-#       if i == MT[n][0]:
-#         MessInBinHuff + MT[n][1]
-#   return MessInBinHuff
-
 freq = buildFrequencyList("apple pie")
 # print(freq)
 tree = buildHuffmanTree(freq)
 # print(tree)
 # toSVG(tree, "tree")
 code = codeDict(tree)
-print("Corentin")
 print(code)
-
-
-
-#################
-#
-# def buildHuffmanTree(inputList):
-#     """
-#     Processes the frequency list into a Huffman tree according to the algorithm.
-#     """
-#     inputList = _ListTupletoBinTree(inputList)
-#     x=0
-#     return _buildHuffmanTree(inputList,0)
-#
-# def _quickSort(L):
-#     less = []
-#     pivotList = []
-#     more = []
-#     if (L==[]):
-#         return L
-#     else:
-#         pivot = L[0].key[0]
-#         for i in L:
-#             if i.key[0] > pivot:
-#                 more.append(i)
-#             elif i.key[0] < pivot:
-#                 less.append(i)
-#             else:
-#                 pivotList.append(i)
-#         less = _quickSort(less)
-#         more = _quickSort(more)
-#         return more + pivotList + less
-#
-# def _ListTupletoBinTree(L):
-#     l=[]
-#     for i in L:
-#         T=BinTree(i,None,None)
-#         l.append(T)
-#     return l
-#
-# def _buildHuffmanTree(L,x):
-#     if(len(L)<=1):
-#         L[0].key ='.'
-#         return L[0]
-#     elif(L==[]):
-#         return None
-#     L=_quickSort(L)
-#     right = L.pop()
-#     left = L.pop()
-#     x+=1
-#     T=BinTree((right.key[0]+left.key[0],x),left,right)
-#     T.left.key = left.key[1]
-#     T.right.key = right.key[1]
-#     L.append(T)
-#     return _buildHuffmanTree(L,x)
-#
-# def encodeData(dataIN, huffmanTree):
-#     """
-#     Encodes the input string to its binary string representation.
-#     """
-#     L=[]
-#     s=""
-#     Mapletable=_mapletable(huffmanTree,L,"")
-#     for i in dataIN:
-#         for j in Mapletable:
-#             if i==j[0]:
-#                 s+=j[1]
-#     return s
-#
-# def _mapletable(B,L,s):
-#     if B!= None:
-#         print(B.key)
-#         if B.left == None and B.right == None:
-#             print(B.key)
-#             L.append((B.key,s))
-#         _mapletable(B.left,L,s+"0")
-#         _mapletable(B.right,L,s+"1")
-#     return L
-#
-# freq = buildFrequencyList("apple pie")
-# print(freq)
-# print(encodeData("apple pie", buildHuffmanTree(freq)))
+print(encodeData("apple pie", tree))
