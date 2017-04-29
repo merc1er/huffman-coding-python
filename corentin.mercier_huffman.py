93c7a879f09610f604b1798bf5033a5eafeec737
Kali <corentin.mercier@epita.fr>
2017-04-30 00:21:20 +0200

    function codeDict working

--- corentin.mercier_huffman.py
+++ corentin.mercier_huffman.py
@@ -105,40 +105,31 @@ def encodeData(dataIN, huffmanTree): # Not working
     """
     pass
 
-def codeDict(tree, l = [], code = ""):
+def codeDict(tree):
     """
     Returns a list of tuples (letter,corresponding code)
     """
-    if tree != None:
-        _tree = tree
-        if tree.left != None:
-            code += "0"
-            codeDict(tree.left, l, code)
-        elif tree.right != None:
-            code += "1"
-            codeDict(tree.right, l, code)
-        else:
-            l.append((tree.key, code))
-        code += "1"
-        return codeDict(_tree.right, l, code)
+    l = []
+    def dfsInfix(tree, string):
+        '''
+        Depth-first traversal
+        Prints keys in inorder
+        '''
+        if tree != None:
+            dfsInfix(tree.left, string + '0')
+            if tree.left == None and tree.right == None:
+              l.append((tree.key, string))
+            dfsInfix(tree.right, string + '1')
+    ms = ''
+    dfsInfix(tree, ms)
     return l
 
-def dfsInfix(B):
-    '''
-    Depth-first traversal
-    Prints keys in inorder
-    '''
-    if B != None:
-        dfsInfix(B.left)
-        print(B.key, end=' ')
-        dfsInfix(B.right)
-
-def _MappingTable (B):
+def _MappingTable(B):
   l=[]
   def dfsPrefix(B, s):
       if B != None:
           if B.left == None and B.right == None:
-            l.append((B.key[0], s))
+            l.append((B.key, s))
           dfsPrefix(B.left, s+'0')
           dfsPrefix(B.right, s+'1')
   ms = ''
@@ -234,3 +225,84 @@ print("Gael")
 print(code2)
 print("Corentin")
 print(code)
+
+
+
+#################
+#
+# def buildHuffmanTree(inputList):
+#     """
+#     Processes the frequency list into a Huffman tree according to the algorithm.
+#     """
+#     inputList = _ListTupletoBinTree(inputList)
+#     x=0
+#     return _buildHuffmanTree(inputList,0)
+#
+# def _quickSort(L):
+#     less = []
+#     pivotList = []
+#     more = []
+#     if (L==[]):
+#         return L
+#     else:
+#         pivot = L[0].key[0]
+#         for i in L:
+#             if i.key[0] > pivot:
+#                 more.append(i)
+#             elif i.key[0] < pivot:
+#                 less.append(i)
+#             else:
+#                 pivotList.append(i)
+#         less = _quickSort(less)
+#         more = _quickSort(more)
+#         return more + pivotList + less
+#
+# def _ListTupletoBinTree(L):
+#     l=[]
+#     for i in L:
+#         T=BinTree(i,None,None)
+#         l.append(T)
+#     return l
+#
+# def _buildHuffmanTree(L,x):
+#     if(len(L)<=1):
+#         L[0].key ='.'
+#         return L[0]
+#     elif(L==[]):
+#         return None
+#     L=_quickSort(L)
+#     right = L.pop()
+#     left = L.pop()
+#     x+=1
+#     T=BinTree((right.key[0]+left.key[0],x),left,right)
+#     T.left.key = left.key[1]
+#     T.right.key = right.key[1]
+#     L.append(T)
+#     return _buildHuffmanTree(L,x)
+#
+# def encodeData(dataIN, huffmanTree):
+#     """
+#     Encodes the input string to its binary string representation.
+#     """
+#     L=[]
+#     s=""
+#     Mapletable=_mapletable(huffmanTree,L,"")
+#     for i in dataIN:
+#         for j in Mapletable:
+#             if i==j[0]:
+#                 s+=j[1]
+#     return s
+#
+# def _mapletable(B,L,s):
+#     if B!= None:
+#         print(B.key)
+#         if B.left == None and B.right == None:
+#             print(B.key)
+#             L.append((B.key,s))
+#         _mapletable(B.left,L,s+"0")
+#         _mapletable(B.right,L,s+"1")
+#     return L
+#
+# freq = buildFrequencyList("apple pie")
+# print(freq)
+# print(encodeData("apple pie", buildHuffmanTree(freq)))
